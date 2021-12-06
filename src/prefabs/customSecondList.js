cbd85a921a55f361096cd733f9589b4d269007cb
Maarten Geerse <mgeerse@gmail.com>
2021-12-06 16:46:18 +0100

    Setup second list component

---
+++ src/prefabs/customSecondList.js
@@ -0,0 +1,12 @@
+(() => ({
+  name: 'Custom Second List',
+  icon: 'ListIcon',
+  category: 'CONTENT',
+  structure: [
+    {
+      name: 'Custom Second List',
+      options: [],
+      descendants: [],
+    },
+  ],
+}))();
