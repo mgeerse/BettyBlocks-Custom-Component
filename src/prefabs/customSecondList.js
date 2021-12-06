d8807fd474fd9f94d276e2f2585b60b2f062ff36
Maarten Geerse <mgeerse@gmail.com>
2021-12-06 16:47:10 +0100

    Setup second list component

--- src/prefabs/customSecondList.js
+++ src/prefabs/customSecondList.js
@@ -1,10 +1,10 @@
 (() => ({
-  name: 'Custom Second List',
-  icon: 'ListIcon',
+  name: 'Second List Component',
+  icon: 'IncludeIcon',
   category: 'CONTENT',
   structure: [
     {
-      name: 'Custom Second List',
+      name: 'Second List Component',
       options: [],
       descendants: [],
     },
